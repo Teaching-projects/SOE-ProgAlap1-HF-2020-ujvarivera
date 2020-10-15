ea7fc0138e5420cb2912b5f8e7bce0f5ab11c89c
ujvarivera <71121119+ujvarivera@users.noreply.github.com>
2020-10-15 19:38:32 +0200

    Az első három feladat megoldva

--- 001/main.py
+++ 001/main.py
@@ -1,4 +1,10 @@
 """
 Kerj be ket egesz szamot, ird ki a nagyobbikat.
 """
+x = int(input("Kérem az első számot! "))
+y = int(input("Kérem a második számot! "))
 
+if x > y:
+    print("A nagyobbik szám: ",x)
+else:
+    print("A nagyobbik szám: ",y)
--- 002/main.py
+++ 002/main.py
@@ -1,3 +1,7 @@
 """
 Kerj be egy szamot, ird ki a ketszereset.
 """
+
+x = int(input("Kérem a számot! "))
+print("A szám kétszerese: ", 2*x)
+
