da2701dc7486a19e5baf3e048496788b432c5fbf
hegyhati <hegyhati.work@gmail.com>
2020-10-15 15:16:11 +0200

    Proper deploy

--- scripts/deploy_exercises.sh
+++ scripts/deploy_exercises.sh
@@ -1,8 +1,13 @@
 source `dirname $0`/remotes.sh
 
+TMP="temporary_branch_to_deploy_to_remotes"
+
 for remote in ${remotes[@]}; do
     cowsay "Deploying new exercises to $remote"
-    git push $remote
+    git branch -d $TMP
+    git checkout -b $TMP $remote/main
+    git merge origin/main && git push $remote HEAD:main
+    git checkout origin/main
 done
 
 
