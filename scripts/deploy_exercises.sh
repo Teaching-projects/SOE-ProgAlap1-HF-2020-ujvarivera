1803afbcd05cccb14b1afb85479205d440b5072b
hegyhati <hegyhati.work@gmail.com>
2020-10-15 15:27:40 +0200

    Pimp deploy script

--- scripts/deploy_exercises.sh
+++ scripts/deploy_exercises.sh
@@ -1,13 +1,26 @@
-source `dirname $0`/remotes.sh
+if [ -z "$(git status --porcelain)" ]; then 
+    BRANCH=$(git rev-parse --abbrev-ref HEAD)
+    if [[ "$BRANCH" != "main" ]]; then
+        echo "Wrong branch"
+        exit 1
+    fi
 
-TMP="temporary_branch_to_deploy_to_remotes"
+    cowsay "First push changes to origin if not yet there..."
+    git push origin
 
-for remote in ${remotes[@]}; do
-    cowsay "Deploying new exercises to $remote"
-    git branch -d $TMP
-    git checkout -b $TMP $remote/main
-    git merge origin/main && git push $remote HEAD:main
-    git checkout main
-done
+    source `dirname $0`/remotes.sh
+    TMP="temporary_branch_to_deploy_to_remotes"
+    
+    for remote in ${remotes[@]}; do
+        cowsay "Deploying new exercises to $remote"
+        git branch -d $TMP
+        git checkout -b $TMP $remote/main
+        git merge origin/main && git push $remote HEAD:main
+        git checkout main
+    done
+else
+    echo "Commit changes first"
+    exit 2
+fi 
 
 
