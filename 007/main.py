d537e5fff9829b20b203c5c59afc48f651cf8267
ujvarivera <71121119+ujvarivera@users.noreply.github.com>
2020-10-23 19:43:23 +0200

    probalkozas

--- 007/main.py
+++ 007/main.py
@@ -3,10 +3,12 @@ egyenleg = 0
 szamldij = 2000
 kamat = 1.1  #ha az egyenleg negativ
 megtakaritas = 1.05 # ha pozitiv
-zaroegyenleg = 0
+osszeg = 0
 
 penzmozgas = int(input()) 
 egyenleg += penzmozgas  #januarban nincs semmi levonas
+osszeg += penzmozgas
+
 if egyenleg > 0:
   egyenleg *= 1.05
 else:
@@ -16,8 +18,9 @@ egyenleg = int(egyenleg)
 
 i = 0
 
-while i < 2:
+while i < 11:
     penzmozgas = int(input())
+    osszeg += penzmozgas
     if penzmozgas > 0:
         egyenleg -= szamldij   #honap eleje
         egyenleg += penzmozgas
@@ -34,3 +37,4 @@ while i < 2:
     i += 1
 
 print(egyenleg)
+print(osszeg)
