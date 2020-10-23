5b8c5ffd4bb70b26e47bdb7e9e019f33f6f8bf79
ujvarivera <71121119+ujvarivera@users.noreply.github.com>
2020-10-23 20:16:34 +0200

    hf kész

--- 007/main.py
+++ 007/main.py
@@ -14,26 +14,19 @@ if egyenleg > 0:
 else:
   egyenleg *= 1.1
 egyenleg = int(egyenleg)
-# print(egyenleg)
 
 i = 0
 
 while i < 11:
     penzmozgas = int(input())
     osszeg += penzmozgas
-    if penzmozgas > 0:
-        egyenleg -= szamldij   #honap eleje
-        egyenleg += penzmozgas
-        egyenleg *= 1.05  #honap vege
-        egyenleg = int(egyenleg)
-        # print(egyenleg)
+    egyenleg -= szamldij
+    egyenleg +=penzmozgas
+    if egyenleg > 0:
+        egyenleg *= 1.05  
     else:
-        egyenleg -= szamldij 
-        egyenleg += penzmozgas
         egyenleg *= 1.1
-        egyenleg = int(egyenleg)
-        # print(egyenleg)
-
+    egyenleg = int(egyenleg)
     i += 1
 
 print(egyenleg)
