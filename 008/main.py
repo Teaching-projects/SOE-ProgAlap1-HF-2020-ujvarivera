b5c216a0b66d58fc671f117514af0ec85b39cde7
ujvarivera <71121119+ujvarivera@users.noreply.github.com>
2020-10-25 16:18:27 +0100

    Kész

--- 008/main.py
+++ 008/main.py
@@ -1,13 +1,23 @@
 x=0.0
 y=0.0
 
+bemenet = None
 
-
-
-
-
+while bemenet != "stop":
+    bemenet = str(input())
+    if bemenet != "stop":
+        szam = float(input())
+    if bemenet == "forward":
+        y += float(szam)
+    elif bemenet == "backward":
+        y -= float(szam)
+    elif bemenet == "left":
+        x -= float(szam)
+    elif bemenet == "right":
+        x += float(szam)
 
 
 print(round(x,2))
 print(round(y,2))
-    
+tav = ((x**2)+(y**2))**(1/2)
+print(round(tav,2))
