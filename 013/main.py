22ad951440660bb51902e26de04de941d6eb9f4e
ujvarivera <71121119+ujvarivera@users.noreply.github.com>
2020-11-11 13:43:31 +0100

    .

--- 013/main.py
+++ 013/main.py
@@ -34,11 +34,34 @@ Ket dolog valtozott meg:
 
 def initialize_map (width, height):
     # ide masold be a helyes megoldasodat a multkorirol
+    lista = [width * ["██"]]
+
+    for x in range(height-2):
+        sor = []
+        for y in range(width):
+            sor.append("░░")
+        sor[0] = "██"
+        sor[-1] = "██"
+        lista.append(sor)
+    lista.append(width*["██"])
+
+    return lista
 
 def pretty_map_print(map, character):
     # Ide masold be a multkorit, a fenti modositasokkal. 
     # Ha a karakter pozicioja a palyan kivul lenne, egyszeruen ne jelenjen meg
+    x = character["position"]["x"]
+    y = character["position"]["y"]
+    width = len(map[1])
+    height = len(map)
 
+    if (x <= width - 1 and x >= 0) and (y <= height - 1 and y >= 0): 
+        map[y][x] = "🧙‍"
+
+    for a in range(len(map)):
+        for b in range(len(map[a])):
+            print(map[a][b],end="")
+        print()
 
 ###############################################################
 ###############################################################
